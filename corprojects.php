9c28515e82ccc9e1b57850e5fdb06ca245cc4eaa
code-11 <brendan.ritter@yahoo.com>
2015-12-31 12:39:14 -0500

    Finished the corporate project section.

--- corprojects.php
+++ corprojects.php
@@ -13,7 +13,7 @@
 			
 			<img class="snippetimage corpimage" src="Pictures/mitre.png" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I interned as a developer at MITRE during the summer of <span class="white">2012</span>. Under the Center for Advanced Aviation System Development, I wrote scripts to analyize both individual flight paths and routes. Further work was done creating an automated documentation system. <br/><br/> Work was done in <span class="blue">R, Matlab</span> and<span class="blue"> Java</span>.</p>
+				<p class="snippettext">I interned as a developer at MITRE during the summer of <span class="white">2012</span>. Under the Center for Advanced Aviation System Development, I wrote scripts to analyize both individual flight paths and routes. Further work was done creating an <span class="green">automated documentation system</span>. <br/><br/> Work was done in <span class="blue">R, Matlab</span> and<span class="blue"> Java</span>.</p>
 			</div>
 		</div>
 
@@ -22,28 +22,28 @@
 			
 			<img class="snippetimage corpimage" src="Pictures/barrett.jpg" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I interned as a developer at Barrett during the summer of <span class="white">2013</span>. I co-lead a small team to create an API for controlling their robotic hand using python. Our team also created a front end for the hand which is still shipped with the product to customers.<br/><br/> Work was done in <span class="blue">python</span>.</p>
+				<p class="snippettext">I interned as a developer at Barrett during the summer of <span class="white">2013</span>. I co-lead a small team to create an <span class="green">API</span> for controlling their robotic hand using python. Our team also created a <span class="green">front end</span> for the hand which is still shipped with the product to customers.<br/><br/> Work was done in <span class="blue">python</span>.</p>
 			</div>
 		</div><div class="snippetbox">
 			
 			
 			<img class="snippetimage corpimage" src="Pictures/microsoft.jpg" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I interned as a developer at Microsoft during the summer of <span class="white">2014</span>. I worked with the Skype calling team to make a experimental in-browser client.<br/><br/> Work was done in <span class="blue">C#</span>  and <span class="blue">javascript</span> </p>
+				<p class="snippettext">I interned as a developer at Microsoft during the summer of <span class="white">2014</span>. I worked with the Skype calling team to make an <span class="green"> experimental in-browser client</span>.<br/><br/> Work was done in <span class="blue">C#</span>  and <span class="blue">javascript</span> </p>
 			</div>
 		</div><div class="snippetbox">
 			
 			
 			<img class="snippetimage corpimage" src="Pictures/rockwell.jpg" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I conducted a senior project with Rockwell (SCOPE). I worked with a multidisciplinary team of five to make an virtual reality simulation of a factory in which we implemented a prototype of a product.<br/><br/> Work was done in <span class="blue">C#</span> and utilized the  <span class="blue">Unity Engine</span> as well as the  <span class="blue">Oculus Rift</span>.</p>
+				<p class="snippettext">I conducted a senior project with Rockwell (SCOPE). I worked with a multidisciplinary team of five to make an <span class="green">virtual reality simulation</span> of a factory in which we implemented a <span class="green">prototype of a product</span>.<br/><br/> Work was done in <span class="blue">C#</span> and utilized the  <span class="blue">Unity Engine</span> as well as the  <span class="blue">Oculus Rift</span>.</p>
 			</div>
 		</div><div class="snippetbox">
 			
 			
 			<img class="snippetimage corpimage" src="Pictures/athena.jpg" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I interned as a developer at AthenaHealth during the summer of <span class="white">2015</span>. I created a code evaluation tool and dashboard.<br/><br/> Work was done in <span class="blue">Perl</span> and <span class="blue">javascript</span>.</p>
+				<p class="snippettext">I interned as a developer at AthenaHealth during the summer of <span class="white">2015</span>. I created a <span class="green">code evaluation tool</span> and dashboard.<br/><br/> Work was done in <span class="blue">Perl</span> and <span class="blue">javascript</span>.</p>
 			</div>
 		</div><div class="snippetbox">
 			
@@ -51,7 +51,7 @@
 			
 			<img class="snippetimage corpimage" src="Pictures/bloomberg.jpg" />
 			<div class= "snippettextbox">
-				<p class="snippettext">I conducted a project with Bloomberd where I worked with a multidisciplinary team of six in order to productize and improve sarcasm detection. We focused on iTunes app ratings and acheived an F1 score of 70%.<br/><br/> Work was done in <span class="blue">python</span> and <span class="blue">javascript</span>.</p>
+				<p class="snippettext">I conducted a project with Bloomberg where I worked with a multidisciplinary team of six in order to productize and improve <span class="green">sarcasm detection</span>. We focused on iTunes app ratings and acheived an F1 score of 70%.<br/><br/> Work was done in <span class="blue">python</span> and <span class="blue">javascript</span>.</p>
 			</div>
 		</div>
 	</div>	
